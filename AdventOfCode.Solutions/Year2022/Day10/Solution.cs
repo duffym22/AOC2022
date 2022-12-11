c16d3c2ee00134f69a1c1c51bf5887779870ac12
Matthew Duffy <duffym22@gmail.com>
2022-12-11 00:36:01 -0600

    - Day 10 part 1 solved

--- AdventOfCode.Solutions/Year2022/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2022/Day10/Solution.cs
@@ -1,3 +1,6 @@
+using Microsoft.Win32;
+using System.Net.Http.Headers;
+
 namespace AdventOfCode.Solutions.Year2022.Day10;
 
 class Solution : SolutionBase
@@ -6,7 +9,56 @@ class Solution : SolutionBase
 
     protected override string SolvePartOne()
     {
-        return "";
+        List<string> lines = Input.SplitByNewline().ToList();
+        List<int> cycleValues = new();
+
+        int registerX = 1;
+
+        bool
+           addxUnblocked = false,
+           addxExecuting = true;
+
+        foreach (string item in lines)
+        {
+            if (item.Contains("noop"))
+            {
+                //noop - 1 cycle
+                cycleValues.Add(registerX);
+            }
+            else
+            {
+                //addx - 2 cycles
+                addxExecuting = true;
+                while (addxExecuting)
+                {
+                    if (addxUnblocked)
+                    {
+                        int opVal = int.Parse(item.Split(' ').Last());
+                        cycleValues.Add(registerX);
+                        registerX += opVal;
+                        addxUnblocked = false;
+                        addxExecuting = false;
+                    }
+                    else
+                    {
+                        cycleValues.Add(registerX);
+                        addxUnblocked = true;
+                    }
+                }
+            }
+        }
+
+        int cycleSums = 0;
+        for (int i = 20; i < 221; i += 40)
+        {
+            int v = cycleValues[i - 1];
+            int product = i * v;
+            cycleSums += product;
+        }
+
+        //Attempt 1: 14160 (correct)
+        return cycleSums.ToString();
+
     }
 
     protected override string SolvePartTwo()
